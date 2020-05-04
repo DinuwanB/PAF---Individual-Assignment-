17fecf518ed7e0394cfd27a256c97a7addf064f5
DinuwanB <dinubhagya97@gmail.com>
2020-05-04 10:21:29 +0530

    Servlet delete and update method done

--- HealthManagement/src/main/java/com/pafProject/HealthManagement/Patient/PatientApi.java
+++ HealthManagement/src/main/java/com/pafProject/HealthManagement/Patient/PatientApi.java
@@ -1,6 +1,10 @@
 package com.pafProject.HealthManagement.Patient;
 
 import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Scanner;
+
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -29,15 +33,54 @@ public class PatientAPI extends HttpServlet {
 		
 		response.getWriter().write(output);
 	}
+	
+	private static Map getParsMap(HttpServletRequest request)
+	{
+		Map<String, String> map = new HashMap<String, String>();
+		
+		try {
+			Scanner scan = new Scanner(request.getInputStream(), "UTF-8");
+			String queryString = scan.hasNext() ? scan.useDelimiter("\\A").next() : "";
+			scan.close();
+			
+			String[] params = queryString.split("&");
+			for (String param : params) {
+				
+				String[] p = param.split("=");
+				map.put(p[0],p[1]);
+			}
+			
+		}
+		catch(Exception e) {
+			System.out.println("MAP Method    " + e);
+		}
+		
+		return map;
+	}
 
 	
 	protected void doPut(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
+		
+		Map paras = getParsMap(request);
+		
+		String output = item.updateUser(paras.get("hidPatSave").toString(), paras.get("nic").toString(),
+				paras.get("f_name").toString(), paras.get("l_name").toString(), paras.get("pat_mail").toString(),
+				paras.get("mob_num").toString(), paras.get("p_bday").toString(), paras.get("addr").toString(), 
+				paras.get("pass").toString());
+		
+		response.getWriter().write(output);
 	}
 
 	
 	protected void doDelete(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
+		
+		Map paras = getParsMap(request);
+		
+		String output = item.delete(paras.get("patID").toString());
+		
+		response.getWriter().write(output);
+		
+		
 	}
 
 }
